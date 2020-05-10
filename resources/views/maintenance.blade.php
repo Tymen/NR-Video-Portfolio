6d63d40c41864168d38f6bce020ec9f215ef4897
Student Tymen Vis <tvis0933@student.landstede.nl>
2020-05-10 16:08:31 +0200

    ddd

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -6,7 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Document</title>
+    <link rel="apple-touch-icon" sizes="57x57" href={{asset("../images/favicon/apple-icon-57x57.png")}}>
+    <link rel="apple-touch-icon" sizes="60x60" href={{asset("../images/favicon/apple-icon-60x60.png")}}>
+    <link rel="apple-touch-icon" sizes="72x72" href={{asset("../images/favicon/apple-icon-72x72.png")}}>
+    <link rel="apple-touch-icon" sizes="76x76" href={{asset("../images/favicon/apple-icon-76x76.png")}}>
+    <link rel="apple-touch-icon" sizes="114x114" href={{asset("../images/favicon/apple-icon-114x114.png")}}>
+    <link rel="apple-touch-icon" sizes="120x120" href={{asset("../images/favicon/apple-icon-120x120.png")}}>
+    <link rel="apple-touch-icon" sizes="144x144" href={{asset("../images/favicon/apple-icon-144x144.png")}}>
+    <link rel="apple-touch-icon" sizes="152x152" href={{asset("../images/favicon/apple-icon-152x152.png")}}>
+    <link rel="apple-touch-icon" sizes="180x180" href={{asset("../images/favicon/apple-icon-180x180.png")}}>
+    <link rel="icon" type="image/png" sizes="192x192"  href={{asset("../images/favicon/android-icon-192x192.png")}}>
+    <link rel="icon" type="image/png" sizes="32x32" href={{asset("../images/favicon/favicon-32x32.png")}}>
+    <link rel="icon" type="image/png" sizes="96x96" href={{asset("../images/favicon/favicon-96x96.png")}}>
+    <link rel="icon" type="image/png" sizes="16x16" href={{asset("../images/favicon/favicon-16x16.png")}}>
+    <link rel="manifest" href="/manifest.json">
+    <title>NR Video - Onderhoud</title>
 </head>
 <style>
     * {
@@ -52,6 +66,7 @@
             <div>
                 <img src="{{asset("../images/logo_outlines_wit-01.png")}}" width="20%">
             <h1>De website is momenteel in onderhoud</h1>
+                <p>Kom later terug!</p>
             <a href="mailto:info@nrvideo.nl" class="fa fa-envelope"></a>
             <a href="#" class="fa fa-instagram"></a>
             <p>Powered by <a href="https://tvis.nl" class="link">Tymen Vis - WebDeveloper</a></p>
