1ba32f0b42896bb0c812ec6881e04712fd77725a
Student Tymen Vis <tvis0933@student.landstede.nl>
2020-05-12 17:01:51 +0200

    added fix for maintenance route

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,17 @@
 @extends("layouts.admin")
 @section("docTitle")
-    Settings
+    Dashboard
 @endsection
 @section("title")
-    Settings
+    Dashboard
 @endsection
 @section("content")
-    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+    <div class="col col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
         <div class="card">
             <div class="card-body">
-                <a class="btn btn-primary text-white" href="/admin/settings">Settings</a>
                 <div class="d-inline-block">
                     <h5 class="text-muted">Website online:</h5>
+                    <a class="btn btn-primary text-white" href="/admin/settings">Settings</a>
                 </div>
                 <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
                     @if($settingsData->maintenance)
@@ -19,6 +19,16 @@
                     @else
                         <div style="display:inline-block; width: 25px; height: 25px; border-radius: 50%; background-color: greenyellow"></div>
                     @endif
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-inline-block">
+                    <h3 class="text-muted">Go to HomePage</h3>
+                    <a class="btn btn-primary text-white" href="/">Home Page</a>
                 </div>
             </div>
         </div>
