b3fd9d087478dada081afbcc2fbc8d160f4b9aca
Student Tymen Vis <tvis0933@student.landstede.nl>
2020-05-12 16:25:07 +0200

    added fix for maintenance route

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="apple-touch-icon" sizes="57x57" href={{asset("../images/favicon/apple-icon-57x57.png")}}>
+    <link rel="apple-touch-icon" sizes="60x60" href={{asset("../images/favicon/apple-icon-60x60.png")}}>
+    <link rel="apple-touch-icon" sizes="72x72" href={{asset("../images/favicon/apple-icon-72x72.png")}}>
+    <link rel="apple-touch-icon" sizes="76x76" href={{asset("../images/favicon/apple-icon-76x76.png")}}>
+    <link rel="apple-touch-icon" sizes="114x114" href={{asset("../images/favicon/apple-icon-114x114.png")}}>
+    <link rel="apple-touch-icon" sizes="120x120" href={{asset("../images/favicon/apple-icon-120x120.png")}}>
+    <link rel="apple-touch-icon" sizes="144x144" href={{asset("../images/favicon/apple-icon-144x144.png")}}>
+    <link rel="apple-touch-icon" sizes="152x152" href={{asset("../images/favicon/apple-icon-152x152.png")}}>
+    <link rel="apple-touch-icon" sizes="180x180" href={{asset("../images/favicon/apple-icon-180x180.png")}}>
+    <link rel="icon" type="image/png" sizes="192x192"  href={{asset("../images/favicon/android-icon-192x192.png")}}>
+    <link rel="icon" type="image/png" sizes="32x32" href={{asset("../images/favicon/favicon-32x32.png")}}>
+    <link rel="icon" type="image/png" sizes="96x96" href={{asset("../images/favicon/favicon-96x96.png")}}>
+    <link rel="icon" type="image/png" sizes="16x16" href={{asset("../images/favicon/favicon-16x16.png")}}>
+    <link rel="manifest" href="/manifest.json">
+    <title>NR Video - Onderhoud</title>
+</head>
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+    }
+    .center{
+        display: flex;
+        height: 200px;
+        margin: auto;
+        border-radius: 10px;
+        min-height: 100vh;
+        background-color: rgba(0, 0, 0, 0.67);
+    }
+    .center div {
+        margin: auto; /* Important */
+        text-align: center;
+    }
+    .fa {
+        padding: 20px;
+        font-size: 30px;
+        width: 30px;
+        text-align: center;
+        text-decoration: none;
+        margin: 5px 2px;
+        border-radius: 50%;
+    }
+
+    .fa:hover {
+        opacity: 0.7;
+    }
+    .fa-envelope, .fa-instagram {
+        background: #00b2ff;
+        color: white;
+    }
+    .link, .link:visited{
+        color:white;
+    }
+</style>
+<body>
+<div style="font-family: Arial; text-align: center; color: white; min-height: 100vh; background-image: url('{{asset('../images/WJNW5235No.jpg')}}'); background-position: center; background-size: cover">
+    <div class="center">
+        <div>
+            <img src="{{asset("../images/logo_outlines_wit-01.png")}}" width="20%">
+            <h1>Error 500 - Internal error</h1>
+            <p>Het lijkt er op dat er iets fout gaat. Neem contact op met mij en geef deze fout aan!</p>
+            <pre>
+
+
+
+            </pre>
+            <a href="mailto:info@nrvideo.nl" class="fa fa-envelope"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <p>Powered by <a href="https://tvis.nl" class="link">Tymen Vis - WebDeveloper</a></p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
